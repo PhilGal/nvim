406709ee9660fcb427ba5200fdc95ea66e681f1b
Philipp Galichkin <pgl@celum.com>
2025-05-18 13:06:14 +0200

    configured lualine

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 vim.wo.number = true
+vim.opt["tabstop"] = 2
+vim.opt["shiftwidth"] = 2
 
 require("pgl")
--- lua/pgl/lazy.lua
+++ lua/pgl/lazy.lua
@@ -19,34 +19,42 @@ vim.g.maplocalleader = "\\"
 
 -- LazyVim setup
 require('lazy').setup({
-    {
-        'nvim-telescope/telescope.nvim',
-        version = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-    },
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+	},
+ 
+	{
+		'rose-pine/neovim',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end,
+	},
 
-    {
-        'rose-pine/neovim',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end,
-    },
+	-- Status bar
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function() 
+			require('lualine').setup()
+		end,
+	},	
 
-    -- Syntax parser, highlighting, etc.
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	-- Syntax parser, highlighting, etc.
+	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
-    -- Switch between selected files quickly
-    { 'theprimeagen/harpoon' },
+	-- Switch between selected files quickly
+	{ 'theprimeagen/harpoon' },
 
-    -- Undo
-    { 'mbbill/undotree' },
+	-- Undo
+	{ 'mbbill/undotree' },
 
-    -- Git status
-    { 'tpope/vim-fugitive' },
+	-- Git
+	{ 'tpope/vim-fugitive' },
 
-    -- LSP plugins (add your LSP plugins here)
-    {'neovim/nvim-lspconfig', tag = 'v1.8.0', pin = true},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
+	-- LSP plugins (add your LSP plugins here)
+	{ 'neovim/nvim-lspconfig', tag = 'v1.8.0', pin = true},
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/nvim-cmp' },
 })
-
